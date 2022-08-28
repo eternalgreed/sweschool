8225747c3d61965917525a1d606b7a4f0d7679a0
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-08-28 21:29:20 +0300

    Solution without additional tasks

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -30,40 +30,109 @@ public class LinkedList {
     public ArrayList<Node> findAll(int _value) {
         ArrayList<Node> nodes = new ArrayList<Node>();
         // здесь будет ваш код поиска всех узлов
+        Node ptr = head;
+        while (ptr != null) {
+            if (ptr.value == _value) {
+                nodes.add(ptr);
+            }
+            ptr = ptr.next;
+        }
         return nodes;
     }
 
     public boolean remove(int _value) {
         // здесь будет ваш код удаления одного узла по заданному значению
+        if (head == null) {
+            return false;
+        }
+        if (head.value == _value) {
+            head = head.next;
+            return true;
+        }
         Node ptr = this.head;
-        while (ptr != null) {
+        while (ptr.next != null) {
             if (ptr.next.value == _value) {
                 ptr.next = ptr.next.next;
+                if (ptr.next == null) {
+                    tail = null;
+                }
                 return true;
+            } else {
+                ptr = ptr.next;
             }
-            ptr = ptr.next;
         }
         return false; // если узел был удалён
     }
 
     public void removeAll(int _value) {
+        Node temp = head;
+        while (temp != null && temp.next != null) { //check for rest of the node except first
+            if (temp.next.value == _value) {
+                temp.next = temp.next.next;
+                if (temp.next == null) {
+                    tail = temp;
+                }
+            } else {
+                temp = temp.next;
+            }
+        }
+        if (head != null && head.value == _value) { //If first node also contain the key
+            head = head.next;
+        }
         // здесь будет ваш код удаления всех узлов по заданному значению
     }
 
     public void clear() {
-        // здесь будет ваш код очистки всего списка
+        while (head != null) {
+            head = head.next;
+        }
+        tail = null;
     }
 
     public int count() {
-        return 0; // здесь будет ваш код подсчёта количества элементов в списке
+        int count = 0;
+        Node ptr = head;
+        while (ptr != null) {
+            count++;
+            ptr = ptr.next;
+        }
+        return count; // здесь будет ваш код подсчёта количества элементов в списке
     }
 
     public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        if (_nodeAfter == null) {
+            head = _nodeToInsert;
+        }
+        Node ptr = head;
+        while (ptr != _nodeAfter) {
+            ptr = ptr.next;
+        }
+        Node tmp = ptr.next;
+        ptr.next = _nodeToInsert;
+        _nodeToInsert.next = tmp;
+        if (tmp == null) {
+            tail = _nodeToInsert;
+        }
         // здесь будет ваш код вставки узла после заданного
 
         // если _nodeAfter = null ,
         // добавьте новый элемент первым в списке
     }
+
+   /* void deleteAllNode(int key) {
+        Node temp = head;
+        while (temp != null && temp.next != null) { //check for rest of the node except first
+            if (temp.next.value == key) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        if (head != null && head.value == key) { //If first node also contain the key
+            head = head.next;
+        }
+    }*/
+
 
 }
 
