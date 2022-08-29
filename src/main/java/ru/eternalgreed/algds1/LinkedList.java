f986f45ec0e16ea16be871be013c0cee18746e5d
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-08-29 10:45:33 +0300

    bug fixed

--- src/main/java/ru/eternalgreed/algds1/LinkedList.java
+++ src/main/java/ru/eternalgreed/algds1/LinkedList.java
@@ -1,3 +1,5 @@
+package ru.eternalgreed.algds1;
+
 import java.util.ArrayList;
 
 public class LinkedList {
@@ -7,6 +9,33 @@ public class LinkedList {
     public LinkedList() {
         head = null;
         tail = null;
+    }
+
+    public static LinkedList addTwoLL(LinkedList first, LinkedList second) {
+        Node ptr1 = first.head;
+        Node ptr2 = second.head;
+        int firstSize = 0;
+        int secondSize = 0;
+        while (ptr1 != null) {
+            firstSize++;
+            ptr1 = ptr1.next;
+        }
+        while (ptr2 != null) {
+            secondSize++;
+            ptr2 = ptr2.next;
+        }
+        if (firstSize != secondSize) {
+            return new LinkedList();
+        }
+        LinkedList result = new LinkedList();
+        ptr1 = first.head;
+        ptr2 = second.head;
+        while (ptr1 != null) {
+            result.addInTail(new Node(ptr1.value + ptr2.value));
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return result;
     }
 
     public void addInTail(Node item) {
@@ -29,6 +58,7 @@ public class LinkedList {
 
     public ArrayList<Node> findAll(int _value) {
         ArrayList<Node> nodes = new ArrayList<Node>();
+        // здесь будет ваш код поиска всех узлов
         Node ptr = head;
         while (ptr != null) {
             if (ptr.value == _value) {
@@ -40,6 +70,7 @@ public class LinkedList {
     }
 
     public boolean remove(int _value) {
+        // здесь будет ваш код удаления одного узла по заданному значению
         if (head == null) {
             return false;
         }
@@ -62,12 +93,12 @@ public class LinkedList {
                 ptr = ptr.next;
             }
         }
-        return false;
+        return false; // если узел был удалён
     }
 
     public void removeAll(int _value) {
         Node temp = head;
-        while (temp != null && temp.next != null) {
+        while (temp != null && temp.next != null) { //check for rest of the node except first
             if (temp.next.value == _value) {
                 temp.next = temp.next.next;
                 if (temp.next == null) {
@@ -77,12 +108,13 @@ public class LinkedList {
                 temp = temp.next;
             }
         }
-        if (head != null && head.value == _value) {
+        if (head != null && head.value == _value) { //If first node also contain the key
             head = head.next;
             if (head ==null) {
                 tail = null;
             }
         }
+        // здесь будет ваш код удаления всех узлов по заданному значению
     }
 
     public void clear() {
@@ -99,7 +131,7 @@ public class LinkedList {
             count++;
             ptr = ptr.next;
         }
-        return count;
+        return count; // здесь будет ваш код подсчёта количества элементов в списке
     }
 
     public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
@@ -116,7 +148,27 @@ public class LinkedList {
         if (tmp == null) {
             tail = _nodeToInsert;
         }
+        // здесь будет ваш код вставки узла после заданного
+
+        // если _nodeAfter = null ,
+        // добавьте новый элемент первым в списке
     }
+
+
+    /* void deleteAllNode(int key) {
+         Node temp = head;
+         while (temp != null && temp.next != null) { //check for rest of the node except first
+             if (temp.next.value == key) {
+                 temp.next = temp.next.next;
+             } else {
+                 temp = temp.next;
+             }
+         }
+         if (head != null && head.value == key) { //If first node also contain the key
+             head = head.next;
+         }
+     }*/
+
 
 }
 
