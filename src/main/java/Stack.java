b92ecc885e7bc25b8afa18d1580db11316fd4121
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-09-09 16:02:15 +0300

    stack added

---
+++ src/main/java/Stack.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Stack<T> {
+
+    private final List<T> stack;
+
+    public Stack() {
+        // инициализация внутреннего хранилища стека
+        stack = new ArrayList<>();
+    }
+
+    public int size() {
+        // размер текущего стека
+        return stack.size();
+    }
+
+    public T pop() {
+        // ваш код
+        if (stack.isEmpty()) return null;
+        T poppedElem = stack.get(0);
+        stack.remove(0);
+        return poppedElem;
+    }
+
+    public void push(T val) {
+        // ваш код
+        stack.add(0, val);
+    }
+
+    public T peek() {
+        // ваш код
+        return stack.isEmpty() ? null : stack.get(0);
+    }
+}
