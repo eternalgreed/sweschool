a1ffd5e049468a14473249b5280bba7752ea7d3e
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-09-14 16:33:43 +0300

    Queue task

---
+++ src/main/java/Queue.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Queue<T> {
+
+    private final List<T> queue;
+
+
+    public Queue() {
+        // инициализация внутреннего хранилища очереди
+        queue = new ArrayList<>();
+    }
+
+    public void enqueue(T item) {
+        // вставка в хвост
+        queue.add(item);
+    }
+
+    public T dequeue() {
+        // выдача из головы
+        if (queue.isEmpty()) {
+            return null;
+        }
+        T elem = queue.get(0);
+        queue.remove(0);
+        return elem; // null если очередь пустая
+    }
+
+    public int size() {
+        return queue.size(); // размер очереди
+    }
+
+}
