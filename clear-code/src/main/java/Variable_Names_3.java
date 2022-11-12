293cbfe2c3a9333e72724ce90d908caa9e2aa281
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-11-12 12:00:45 +0300

    Variables 3 solution

---
+++ clear-code/src/main/java/Variable_Names_3.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * 7.1
+ * 1) balanced -> isBalancedBinaryTree
+ * 2) valid -> isLoginValid
+ * 3) flag -> isEntranceFound
+ * 4) check -> wasChecked
+ * 5) grows -> isAscending
+ *
+ * 7.2
+ * isInitialized - initialized
+ * isErrorOccurred - error ?
+ *
+ * 7.3
+ * i - taskNumber
+ * // при итерации по списку задач
+ *
+ * 7.4
+ * 1) startRequestTime - endRequestTime
+ * 2) headPointer - tailPointer
+ * 3) forwardShift - backwardShift (or reverseShift ?)
+ *
+ * 7.5
+ * 1) tmp - temporarySum
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+public class Variable_Names_3 {
+}
