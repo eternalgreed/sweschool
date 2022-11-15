aed6d24931b892e9db641a2da8a316b460227e84
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-11-15 17:29:55 +0300

    Avoid name added

---
+++ clear-code/src/main/java/Variable_Names_4_Avoid.java
@@ -0,0 +1,46 @@
+/**
+ * 1) resultList - filteredList
+ * // список из элементов после применения некоторой фильтрации
+ *
+ * 2) n - matrixDimension
+ * // Размерность матрицы
+ *
+ * 3) distance - distanceInVertexCount
+ * // количество ребер между вершинами графа
+ *
+ * 4) file - contentOfFileString
+ * // вычитанное содержимое файла
+ *
+ * 5)startTime, finishTime - startTimeInMillis , finishTimeMillis
+ * // Измерение времени выполнения блока кода в миллисекундах
+ *
+ * 6) message - xmlMessageString
+ * // ответ в формате xml в строке
+ *
+ * 7) plusSum - positiveNumberSum
+ * //сумма положительных элементов
+ *
+ * 8) node - currentNode
+ * // текущая нода при итерации
+ *
+ * 9) counter - totalNodesNum
+ *  // количество нод в дереве
+ *
+ * 10) error - errorDescriptionString
+ *  //строка, содержащая описание ошибки
+ *
+ * 11) requestThreshold - requestCountThresholdAverage
+ * // среднее порговое значение запросов
+ *
+ * 12) tokenDuration - keepAliveTokenDurationMinutes
+ * // продолжительность жизни токена в минутах
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+public class Variable_Names_4_Avoid {
+}
