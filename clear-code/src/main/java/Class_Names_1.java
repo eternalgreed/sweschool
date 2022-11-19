5cce10c82a3f6445f4313d00039c1ba10a345148
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-11-19 18:32:27 +0300

    Class names 1

---
+++ clear-code/src/main/java/Class_Names_1.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * 3.1. Улучшите пять имён классов в вашем коде.
+ *
+ * LinkedList2 - TwiceLinkedList
+ * LinkedList - SingleLinkedList
+ * DynArray - DynamicSizeArray
+ * PasswordCheck - PasswordValidator
+ * EventCollection - EventQueue
+ *
+ *
+ * 3.2. Улучшите семь имён методов и объектов
+ *
+ * 1. public void enqueue - public void addItemInTail(T item)
+ * // метод добавление элемента в хвост
+ * 2. public T dequeue() - public T extractFromHead ()
+ * // метод извлечения элемента из головы очереди
+ *
+ * 3. public void addFront(T item) -  public void addItemInHead(T item)
+ *  // метод добавления элемента в голову
+ * 4. public voi addTail() - public voi addItemInHead()
+ * // метод добавление элемента в хвост
+ * 5. public T removeFront()  - public T deleteInHeadAndReturn()
+ * // метод удаление элемента из головы
+ * 6. public T removeTail() - public T deleteInTailAndReturn()
+ * // метод удаление элемента из хвоста
+ * 7. public void actionsAfterSend() - public void resetSettingsAfterSend()
+ * // метод сбрасывающий некоторые настройки после отправки события
+ *
+ *
+ *
+ *
+ *
+
+ *
+ */
+
+
+public class Class_Names_1 {
+}
