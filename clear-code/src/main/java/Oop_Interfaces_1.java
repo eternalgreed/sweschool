b9c44bd58c20ee9823a37a2e083f8273314d6d48
Ramazan Abdullaev <eternalgreed99@gmail.com>
2022-11-25 17:36:03 +0300

    Oop and interfaces

---
+++ clear-code/src/main/java/Oop_Interfaces_1.java
@@ -0,0 +1,33 @@
+/**
+ * 3.1
+ * List<Integer> list = List.of(1,2,3)
+ * HashTable hashTable = HashTable.ofClassAndSize(String.class, 32)
+ * BloomFilter bloomFilter = BloomFilter.ofLength(32)
+ *
+ *
+ *
+ *
+ *
+ *
+ * 3.2
+ *
+ * interface UserDao {
+ *     User find(Long id);
+ * }
+ *
+ * class UserDaoImpl implements UserDao {
+ *
+ *     @Override
+ *     public User find(Long id) {
+ *         return null;
+ *     }
+ * }
+ *
+ *
+ */
+
+
+
+
+public class Oop_Interfaces_1 {
+}
