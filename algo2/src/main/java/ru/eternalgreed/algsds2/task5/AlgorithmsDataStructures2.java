f3aca7e51c1700947f8828801279731d4e2a1970
Ramazan Abdullaev <eternalgreed99@gmail.com>
2023-01-21 16:14:49 +0300

    Added balanced tree

---
+++ algo2/src/main/java/ru/eternalgreed/algsds2/task5/AlgorithmsDataStructures2.java
@@ -0,0 +1,37 @@
+package ru.eternalgreed.algsds2.task5;
+
+import java.util.*;
+
+public class AlgorithmsDataStructures2
+{
+    public static int[] GenerateBBSTArray(int[] a)
+    {
+        Arrays.sort(a);
+        int depth = 0;
+        int balancedTreeSize =1;
+        while (balancedTreeSize<=a.length){
+            balancedTreeSize *= 2;
+            depth++;
+        }
+        balancedTreeSize--;
+        int [] resultArr = new int[balancedTreeSize];
+        generateBBSTArrayRec(a, resultArr,0, a.length, 0);
+        return resultArr;
+    }
+
+    private static void generateBBSTArrayRec(int[] a, int[] result, int start, int end, int treeIndex) {
+        if (start >= end) {
+            return;
+        }
+        int middle = start + (end - start)/2;
+        result[treeIndex] = a[middle];
+        int leftChildIndex = 2* treeIndex +1;
+        int rightChildIndex = 2* treeIndex +2;
+        if (leftChildIndex < result.length) {
+            generateBBSTArrayRec(a, result, start, middle, leftChildIndex);
+        }
+        if (rightChildIndex < result.length) {
+            generateBBSTArrayRec(a, result, middle, end,rightChildIndex);
+        }
+    }
+}
